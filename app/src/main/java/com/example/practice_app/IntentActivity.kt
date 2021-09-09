d4c68e6b3dd30e9680c4e0ce723edcc8524d5742
prathamFlobiz <pratham@flobiz.in>
2021-09-09 23:06:51 +0530

    Test

--- app/src/main/java/com/example/practice_app/IntentActivity.kt
+++ app/src/main/java/com/example/practice_app/IntentActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class IntentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //Just time pass
         setContentView(R.layout.activity_intent)
     }
 }
